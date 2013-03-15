faf823cb43f814444f34478f6db33d45df9eafaf
Georg Walther <georgwalther@gmail.com>
2013-03-15 12:53:54 +0000

    Adapted single-layer MAPK system drawing to Dot output

--- draw_single_layer_mapk.py
+++ draw_single_layer_mapk.py
@@ -1,12 +1,12 @@
 from stoich import get_graph_stoich
 from parse_mechanism import get_network_from_mechanism
-from drawing import gratelpy_draw
+from drawing import gratelpy_draw, gratelpy_dot
 from matplotlib import pyplot as plt
 from fragments import get_sensible_fragments
 from subgraphs import get_all_valid_subgraphs
 
 #mechanism_file = '/usr/users/cbu/waltherg/JIC/Dropbox/GratelPy/mechanisms/reversible_substrate_inhibition.txt'
-mechanism_file = '/home/waltherg/Dropbox/GratelPy/mechanisms/single_layer_mapk_mechanism.txt'
+mechanism_file = '../mechanisms/single_layer_mapk_mechanism.txt'
 
 base_name = 'single_layer_mapk'
 
@@ -34,25 +34,4 @@ for s in dict_complexes:
 for w in dict_constants:
     pos['w'+str(dict_constants[w]+1)] = pos_mech[w]
 
-fig = gratelpy_draw(G, positions=pos, dictionary_complexes=dict_complexes_reverse, dictionary_reactions=dict_constants_reverse, rnsize=3600, cnsize=2000)
-plt.draw()
-plt.savefig(base_name+'_graph.pdf', bbox_inches='tight')
-
-valid_fragments = get_sensible_fragments(G, stoich_rank)
-critical_fragments = []
-for frag in valid_fragments:
-    res = get_all_valid_subgraphs(G, stoich_rank, frag)
-    if res[-1] < 0:
-        critical_fragments.append(res)
-print 'number of critical fragments:',str(len(critical_fragments))
-for f_i, f in enumerate(critical_fragments):
-    print f_i, [dict_complexes_reverse[int(s[1:])-1] for s in f[0][0]], [dict_constants_reverse[int(w[1:])-1] for w in f[0][1]]
-
-for frag_i in range(len(critical_fragments)):
-    frag_unfurled = [n for li in critical_fragments[frag_i][0] for n in li]
-    
-    gratelpy_draw(G.subgraph(frag_unfurled), positions=pos, dictionary_complexes=dict_complexes_reverse, dictionary_reactions=dict_constants_reverse)
-    plt.draw()
-    plt.savefig(base_name+'_critical_fragment_'+str(frag_i)+'_'+str(stoich_rank)+'.pdf', bbox_inches='tight')
-    
-plt.show()
+dot_graph = gratelpy_dot(G, positions=pos_mech, dictionary_complexes=dict_complexes_reverse, dictionary_reactions=dict_constants_reverse,filename='single_layer_mapk.dot')
